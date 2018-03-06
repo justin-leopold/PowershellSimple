d02d32a69f2954f776d91e2aa7f3b2c37e9a7e3b
justin-leopold <30728066+justin-leopold@users.noreply.github.com>
2018-03-05 18:22:04 -0700

    Now an advanced functiom/module

--- Windowsgrayloginstallmultiple.ps1
+++ Windowsgrayloginstallmultiple.ps1
@@ -1,31 +1,45 @@
-<#  
-.SYNOPSIS  
-    Install Graylog to windows server using a Foreach loop and data generated from a SW report
-.DESCRIPTION  
-    Installs Graylog and sets default settings for windows 
-    logging to allow configuration management via Graylog Configuration Manager  
-.NOTES  
-    File Name   : Windowsgrayloginstallmultiple.ps1  
-    Author      : Justin Leopold - 7/18/2017 
-    Written on  : Powershell 5.1
-.LINK  
-#>
+Function Install-Graylog {
+    <#  
+    .SYNOPSIS  
+        Install Graylog to windows server using a Foreach loop and data generated from a SW report
+    .DESCRIPTION  
+        Installs Graylog and sets default settings for windows 
+        logging to allow configuration management via Graylog Configuration Manager  
+    .NOTES  
+        File Name   : Windowsgrayloginstallmultiple.ps1  
+        Author      : Justin Leopold - 7/18/2017 
+        Written on  : Powershell 5.1
+    .LINK  
+    #>
 
-#Variables for install
-$Computers = "hostname"
-$configfile = 'collector_sidecar.yml'
-$command = {powershell.exe c:\windows\temp\collector_sidecar.exe /S}
-$path = '\\dpsnas01\softwaredepot\Windows\Microsoft\Windows\Utilities\Graylog\collector_sidecar_installer_0.1.3-1.exe'
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage = "Enter Computer Name(s)")]   
+        [Alias('hostname', 'cn')]
+        [string[]]$ComputerName
+    )
 
-Foreach ($computer in $Computers) {
+    #Static Variables for install
+    #Make config variable based on directory
+    $configfile = '\\dpsnas01\softwaredepot\Windows\Microsoft\Windows\Utilities\Graylog\filebeat\collector_sidecar_exchange2010iis.yml'
+    $command = {powershell.exe c:\windows\temp\collector_sidecar.exe /S}
 
-    Copy-Item -Path '$path' -Destination "\\$computer\c$\windows\temp\collector_sidecar.exe"
+        Foreach ($computer in $ComputerName) {
 
-    Invoke-Command -ComputerName $computer -ScriptBlock $command
+            Copy-Item -Path '\\dpsnas01\softwaredepot\Windows\Microsoft\Windows\Utilities\Graylog\collector_sidecar_installer_0.1.3-1.exe' -Destination "\\$computer\c$\windows\temp\collector_sidecar.exe"
 
-    Copy-Item -Path $configfile -Destination "\\$computer\c$\program files\Graylog\collector-sidecar" -Force
+            Invoke-Command -ComputerName $computer -ScriptBlock $command
 
-    Invoke-Command -ComputerName $computer -ScriptBlock {Start-Process 'C:\Program Files\graylog\collector-sidecar\graylog-collector-sidecar.exe' -ArgumentList "-service", "install"}
-    Invoke-Command -ComputerName $computer -ScriptBlock {Start-Process 'C:\Program Files\graylog\collector-sidecar\graylog-collector-sidecar.exe' -ArgumentList "-service", "start"}
+            Copy-Item -Path $configfile -Destination "\\$computer\c$\program files\Graylog\collector-sidecar" -Force
 
-}#foreach
+            Invoke-Command -ComputerName $computer -ScriptBlock {Start-Process 'C:\Program Files\graylog\collector-sidecar\graylog-collector-sidecar.exe' -ArgumentList "-service", "install"}
+            Invoke-Command -ComputerName $computer -ScriptBlock {Start-Process 'C:\Program Files\graylog\collector-sidecar\graylog-collector-sidecar.exe' -ArgumentList "-service", "start"}
+
+            Remove-Item -Path "\\$computer\c$\program files\Graylog\collector-sidecar\collector_sidecar.yml"
+            Rename-Item -Path "\\$computer\c$\program files\Graylog\collector-sidecar\collector_sidecar_exchange2010iis.yml" -NewName "\\$computer\c$\program files\Graylog\collector-sidecar\collector_sidecar.yml"
+
+        }#foreach
+    }
